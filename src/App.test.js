9c08c18e2bd056afbf527fe800c2b7e3e069afb9
EthanTroel <95850312+EthanTroel@users.noreply.github.com>
2022-03-28 16:32:33 +0200

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,16 +3,19 @@ import { mount } from '@cypress/react';
 import App from './App';
 
 it('renders learn react link', () => {
-  mount(App);
-  cy.get('a').contains('Learn React');
+  mount(<App />);
+  cy.get('a').contains('learn react');
 });
 
 it('count initialized with 0', () => {
-  mount(App);
+  mount(<App />);
   cy.get('[data-cy="count"]').contains('0');
 })
 it('button click increase count by 1', () => {
-  mount(App);
-  cy.get('[data-cy="button"]').click();
+  mount(<App />);
   cy.get('[data-cy="count"]').contains('1');
 })
+
+
+
+
